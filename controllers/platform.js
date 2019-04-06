8268d29f24b5c9fba4d3cc1868d75010deeee1d3
Goncalo Amaral <goa.docs@gmail.com>
2019-04-06 17:06:15 +0100

    AUth util

--- controllers/platform.js
+++ controllers/platform.js
@@ -1,15 +1,10 @@
 import express from 'express'
-import { render_view } from '../utils'
+import { render_view, auth } from '../utils'
 import routes from '../routes.json'
 
 let router = express.Router()
 
-router.use((req, res, next) => {
-  if (!req.session.user_id) {
-    return res.redirect(routes['website_sign_in'])
-  }
-  next()
-})
+router.use(auth)
 
 router.get('/', (_, res) => {
   render_view(res, 'platform/index')
