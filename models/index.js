a3b2da7b837337bc76faa27273ae4aa9961192dd
Catarina Lucas <catarinalucasloureiro@gmail.com>
2019-04-06 19:41:17 +0100

    submit button

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import User from './User'
 
-const DB_URL = 'mongodb://localhost:27017/gogocat'
+const DB_URL = 'mongodb://user:secret19@ds133256.mlab.com:33256/gogocat'
 mongoose.connect(DB_URL, { useNewUrlParser: true });
 
 console.log(`Connected to mongo at ${DB_URL}`)
