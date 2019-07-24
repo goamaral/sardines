91d3ee5f1ae11625c3212f4009e5c9f7f22b264a
Goncalo Amaral <goa.docs@gmail.com>
2019-07-24 04:24:36 +0100

    Admin update user password

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { auth, admin_auth, sanitize_input, render_view, url_for } from '../utils'
+import { auth, admin_auth, sanitize_input, render_view, url_for, hash_password } from '../utils'
 import { User } from '../models'
 
 let router = express.Router()
@@ -48,10 +48,21 @@ router.get('/users/edit/:username', async (req, res) => {
 /* ADMIN USERS UPDATE */
 router.post('/users/edit/:username', async (req, res) => {
   let errors = {}
+  
   let params = {
     username: req.body.username,
     email: req.body.email,
     role: req.body.role
+  }
+
+  if (req.body.password.length > 0 && req.body.password.length < 8) {
+    return render_view(res, 'admin/users/edit', {
+      user: { username: params.username, email: params.email, role: params.role, original_username: req.params.username },
+      User,
+      errors: { password: "Password tem que ter pelo menos 8 caracteres" }
+    })
+  } else {
+    params["password"] = hash_password(req.body.password)
   }
 
   try {
@@ -64,7 +75,7 @@ router.post('/users/edit/:username', async (req, res) => {
         errors[e] = err.errors[e].message
       })
     } else {
-      errors["username"] = "Unknow error"
+      errors["username"] = "Unknown error"
     }
 
     render_view(res, 'admin/users/edit', {
